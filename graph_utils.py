b77f8ee9ab2388a6f8ca4dd6a4f888dd488c0dee
fachmyrz <64829676+fachmyrz@users.noreply.github.com>
2025-10-17 14:12:38 +0700

    Update graph_utils.py

--- graph_utils.py
+++ graph_utils.py
@@ -1,9 +1,7 @@
 import plotly.express as px
-from data_preprocess import compute_all, get_summary_data
+from data_preprocess import get_summary_data, df_visit
 
-computed = compute_all()
-visits = computed["visits"]
-summary, data = get_summary_data(visits)
+summary, data = get_summary_data()
 gb_sum = data.groupby(["month_year","employee_name"], as_index=False).agg({"ctd_visit":"sum","avg_distance_km":"mean","avg_time_between_minute":"mean","avg_speed_kmpm":"median"})
 ctd_visit = px.line(gb_sum, x="month_year", y="ctd_visit", color="employee_name", markers=True)
 avg_distance = px.line(gb_sum, x="month_year", y="avg_distance_km", color="employee_name", markers=True)
